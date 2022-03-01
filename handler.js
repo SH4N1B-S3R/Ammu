e30b84dd93083a497635e71f7e5fa708d375a63b
SH4N1B-S3R <99301665+SH4N1B-S3R@users.noreply.github.com>
2022-03-01 14:56:06 +0530

    SH4NIB

--- handler.js
+++ handler.js
@@ -391,7 +391,7 @@ module.exports = {
             let groupMetadata = await this.groupMetadata(jid)
             for (let user of participants) {
               // let pp = './src/avatar_contact.png'
-              let pp = 'https://telegra.ph/file/fd4230902e75d397d8667.jpg'
+              let pp = 'https://i.imgur.com/BbjxSh2.jpeg'
               try {
                 pp = await uploadImage(await (await fetch(await this.getProfilePicture(user))).buffer())
               } catch (e) {
@@ -478,7 +478,7 @@ global.dfail = (type, m, conn) => {
     private: 'This command can only be used in Private Chat',
     admin: 'This command is only for *Admin* group',
     botAdmin: 'Make bot as *Admin* to use this command',
-    unreg: 'Please register to use this feature by typing:\n\n*#register name.age*\n\nEXAMPLE: *#register Ammu.18*',
+    unreg: 'Please register to use this feature by typing:\n\n*#register name.age*\n\nEXAMPLE: *#register Shanu.18*',
     nsfw: 'NSFW is not active'
   }[type]
   if (msg) return m.reply(msg)
