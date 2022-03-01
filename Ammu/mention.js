dc7c899e6bf33e90a4a2b66162e37a5bacaf2045
SH4N1B-S3R <99301665+SH4N1B-S3R@users.noreply.github.com>
2022-03-01 15:25:11 +0530

    SH4NIB

--- Ammu/mention.js
+++ Ammu/mention.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Example:\n${usedPrefix + command} @919656459062`
+  if (!text) throw `Example:\n${usedPrefix + command} @918075168486`
   m.reply(text, false, {
     contextInfo: {
       mentionedJid: conn.parseMention(text)
